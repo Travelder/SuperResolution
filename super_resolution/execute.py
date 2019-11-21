74bb8d63779f1061e4f2d3da702349de8623453b
Saket <invincible.shukla@gmail.com>
2019-11-21 20:21:13 +0530

    Added a fresh copy of the original ML repo as a submodule. Updated references to match with django' architecture. Created a new method to resolve and return a processed file.

---
+++ super_resolution/execute.py
@@ -0,0 +1,38 @@
+import os
+import matplotlib.pyplot as plt
+
+
+from .data import DIV2K
+from .model.srgan import generator, discriminator
+from .train import SrganTrainer, SrganGeneratorTrainer
+
+from .model import resolve_single
+from .utils import load_image
+dirname = os.path.dirname(__file__)
+weights_dir = os.path.join(dirname,'weights/srgan')
+weights_file = lambda filename: os.path.join(weights_dir, filename)
+os.makedirs(weights_dir, exist_ok=True)
+pre_generator = generator()
+gan_generator = generator()
+pre_generator.load_weights(weights_file('pre_generator.h5'))
+gan_generator.load_weights(weights_file('gan_generator.h5'))
+
+def resolve(lr_image_path):
+    lr = load_image(lr_image_path)
+    
+    pre_sr = resolve_single(pre_generator, lr)
+    gan_sr = resolve_single(gan_generator, lr)
+    
+    images = [lr, pre_sr, gan_sr]
+    titles = ['LR', 'SR (PRE)', 'SR (GAN)']
+    positions = [1, 3, 4]
+    
+    plt.axis('off')
+    plt.imshow(gan_sr)
+    import pdb; pdb.set_trace()
+    fname = '.'.join(lr_image_path.split('.')[:-1])
+    image_name =  fname + '_out' + '.png'
+    
+    plt.savefig(image_name, transparent=True, bbox_inches='tight', pad_inches=0)
+    
+    return image_name
---
+++ super_resolution/model/__init__.py
@@ -0,0 +1,3 @@
+from .common import resolve
+from .common import resolve_single
+from .common import evaluate
